def4d03ecd289338c2dcc1e7f857c1e38c4dce2c
Dhruvil Tank <dhruviltank@192.168.1.9>
2025-07-28 01:21:32 +0530

    day 5 add in to repository

---
+++ day4/ternary.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int subscribe = 10;
+
+    cout << (subscribe < 20 ? 1 :0);
+
+    return 0;
+}
---
+++ day4/while_loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int run_loop_ten_time = 10;
+    int count = 0;
+    while (count < 10 ){
+        count ++;
+        cout << "loop runing time " << count << endl ; 
+    }
+    return 0;
+}
