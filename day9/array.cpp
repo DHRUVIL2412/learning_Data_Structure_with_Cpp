ee3fcaace90e0013970e1f89fb56b4ac2a70b354
Dhruvil Tank <dhruviltank@192.168.1.21>
2025-08-05 15:37:56 +0530

    add day 8

---
+++ day9/array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int marks[] = { 99,198,23,45,6,234,5,46};
+    int samle[5] = {101,13,204,242,202};
+    cout << sizeof(marks);
+    cout << sizeof(samle);
+    return 0;
+}
---
+++ day9/find_min_max.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int find_min(int arr){
+
+    for (int i = 0 ; i <  arr ; i++){
+
+    }
+}
+int main(){
+    int numbs[] = {10,20 ,304,29,393,4929,2342,52,53,1245,12,45,2,5};
+
+
+    return 0;
+}
