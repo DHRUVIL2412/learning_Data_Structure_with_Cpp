3a537c0a5ec51451620ce581d289db0533a381d8
Dhruvil Tank <dhruviltank@192.168.1.2>
2025-08-13 17:26:34 +0530

    add majority element

---
+++ day11/majority_element.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    // try using the moore's algorithm
+    int arr[] = { 1,2,4,3,3,4,4,4};
+
+    for (int i = 0 ;  i < 8 ; i++ ){
+        
+    }
+
+    return 0;
+
+}
